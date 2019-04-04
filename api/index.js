b574c10373fa69335fd4d3d75ab6223b0305aca6
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2019-04-04 01:47:52 -0400

    Added shadow effect with drag

--- api/index.js
+++ api/index.js
@@ -25,10 +25,6 @@ db.once('open', function() {
         res.send('Hello World!')
     })
     app.get('/getRandomCard', (req, res) => {
-        // console.log("lel")
-        // console.log(cardsJson);
-        // console.log(cardsJson);
-
         var Card = mongoose.model('Card', CardSchema);
 
         Card.count().exec((err, count) => {
@@ -36,23 +32,11 @@ db.once('open', function() {
             // Get a random entry
             var random = Math.floor(Math.random() * count)
 
-            // Again query all users but only fetch one offset by our random #
             Card.findOne().skip(random).exec((err, card) => {
-                // Tada! random user
                 console.log("found this card!", card.name);
                 res.send(card);
             })
         });
-
-        // res.send(cardsJson)
-        // const keys = Object.keys(cardsJson)
-        // const key = keys[Math.floor(Math.random() * keys.length)]
-        // console.log(key);
-        // const cards = cardsJson[key].cards;
-        // const card = cards[Math.floor(Math.random() * cards.length)]
-        // console.log(card.name);
-        // const url = "https://img.scryfall.com/cards/large/en/" + key.toLowerCase() + "/" + card.number + ".jpg";
-        // res.send(url)
     })
     
     app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- app/src/components/Card.vue
+++ app/src/components/Card.vue
@@ -38,6 +38,43 @@ export default {
         this.cardX = this.lerp(this.cardX, this.mouseX - 118, 0.05)
         this.cardY = this.lerp(this.cardY, this.mouseY - 50, 0.05)
 
+
+        var rotateYAmt = 0;
+        var rotateXAmt = 0;
+        var shadowH = 0;
+        var shadowV = 11;
+
+        // console.log("mousex - cardx", (this.mouseX - this.cardX) - 118);
+        if (((this.mouseX - this.cardX) - 118) < -20) {
+          rotateYAmt = -25;
+          shadowH = 17;
+        }
+        else if (((this.mouseX - this.cardX) - 118) > 20) {
+          rotateYAmt = 25;
+          shadowH = -17;
+        }
+        else {
+          rotateYAmt = 0;
+          shadowH = 0;
+        }
+
+        if (((this.mouseY - this.cardY) - 50) > 20) {
+          rotateXAmt = 25;
+          shadowV = -28;
+          
+        }
+        else if (((this.mouseY - this.cardY) - 50) < -20) {
+          rotateXAmt = -25;
+          shadowV = 28;
+        }
+        else {
+          rotateXAmt = 0;
+          shadowV = 18;
+        }
+
+        this.$el.style.transform = "rotateX("+ rotateXAmt + "deg) rotateY(" + rotateYAmt + "deg)";
+        this.$el.style.boxShadow = shadowH + "px " + shadowV + "px 18px -2px rgba(0,0,0,0.75)";
+
         this.$el.style.left = this.cardX + 'px'
         this.$el.style.top = this.cardY + 'px'
       }, 60 / 1000)
@@ -48,6 +85,9 @@ export default {
       this.$el.style.left = 'inherit'
       this.$el.style.top = 'inherit'
       this.$el.style.zIndex = 'inherit'
+
+      this.$el.style.transform = "rotateY(0deg)"
+      this.$el.style.boxShadow = "0px 11px 18px -2px rgba(0,0,0,0.75)";
       this.isPressed = false
     },
     lerp (start, end, amt) {
@@ -72,7 +112,7 @@ export default {
         -moz-box-shadow: 0px 11px 18px -2px rgba(0,0,0,0.75);
         box-shadow: 0px 11px 18px -2px rgba(0,0,0,0.75);
         bottom: 0px;
-        transition: width .15s ease-in-out, height .15s ease-in-out, margin .25s ease-in-out;
+        transition: width .15s ease-in-out, height .15s ease-in-out, margin .25s ease-in-out, transform .25s ease-in-out, box-shadow .25s ease-in-out;
         /* position: absolute; */
         /* bottom: 0; */
         /* margin: 0px -30px; */
