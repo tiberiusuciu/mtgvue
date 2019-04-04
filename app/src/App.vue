45c8a57fe62ef61afe3736288e6f12552e34eead
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2019-04-04 16:36:35 -0400

    Added fontawesome, fixed up vue router

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
+      <router-link to="/game">Game</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <!-- <img src="https://img.scryfall.com/cards/large/en/c16/322.jpg?1517813031" width="300" alt="test"> -->
--- app/src/router.js
+++ app/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Game from './views/Game.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/game',
+      name: 'game',
+      component: Game
     },
     {
       path: '/about',
