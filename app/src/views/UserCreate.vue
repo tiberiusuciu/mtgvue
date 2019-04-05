f6501b3be5d9666c78c51b7baec93b7c50429259
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2019-04-05 00:54:14 -0400

    User input styling

--- app/src/views/UserCreate.vue
+++ app/src/views/UserCreate.vue
@@ -5,8 +5,12 @@
             <div class="user-create-form">
                 <h1 class="title">Sign Up</h1>
                 <div class="pill-input">
-                    <div class="left-pill"></div>
-                    <input type="text" class="pill-input"></div>
+                    <div class="left-pill">
+
+                    </div>
+                    <div class="pill-input-area">
+                        <input type="text" class="pill-input-tag"/>
+                    </div>
                     <div class="right-pill"></div>
                 </div>
             </div>
@@ -38,6 +42,7 @@
         height: 100vh;
         background-color: #335;
         background-image: url('https://images.unsplash.com/photo-1554147090-e1221a04a025?ixlib=rb-1.2.1&auto=format&fit=crop&w=1631&q=80');
+        background-size: cover;
     }
     .backdrop {
         position: absolute;
@@ -61,5 +66,63 @@
         -moz-box-shadow: -18px 0px 48px -14px rgba(0,0,0,0.73);
         box-shadow: -18px 0px 48px -14px rgba(0,0,0,0.73);
         border-radius: 5px;
+    }
+    .pill-input {
+        /* width: 100%; */
+        height: 60px;
+        /* background-color: aqua; */
+        display: block;
+        margin-left: 50px;
+        margin-top: 25px;
+    }
+    .pill-input-area {
+        width: 250px;
+        /* background-color: aquamarine; */
+        display: inline-block;
+        height: 60px;
+        vertical-align: top;
+        /* top: 0px; */
+        position: relative;
+    }
+    .pill-input-tag {
+        /* width: 250px; */
+        position: relative;
+        /* background-color: aquamarine; */
+        display: inline-block;
+        height: 60px;
+        padding: 0px;
+        border: 0px;
+        border-top: 2px solid #ccc;
+        border-bottom: 2px solid #ccc;
+        box-sizing: border-box;
+    }
+    input {
+        display: inline-block;
+        width: 100%;
+        background-color: ghostwhite;
+    }
+    .left-pill {
+        /* background-color: aquamarine; */
+        display: inline-block;
+        width: 50px;
+        height: 60px;
+        border-top-left-radius: 50%;
+        border-bottom-left-radius: 50%;
+        border-left: 2px solid #ccc;
+        border-top: 2px solid #ccc;
+        border-bottom: 2px solid #ccc;
+        box-sizing: border-box;
+    }
+    .right-pill {
+        display: inline-block;
+        /* background-color: aquamarine; */
+        width: 50px;
+        height: 60px;
+        border-top-right-radius: 50%;
+        border-bottom-right-radius: 50%;
+        border-right: 2px solid #ccc;
+        border-top: 2px solid #ccc;
+        border-bottom: 2px solid #ccc;
+        box-sizing: border-box;
     }
 </style>
