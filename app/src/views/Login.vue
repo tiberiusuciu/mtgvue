da4ec983eab2a3dec435171ad191b587dc2b9b3b
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2019-04-12 01:49:47 -0400

    Added images locally for login and username page

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -135,7 +135,7 @@
         flex: 1;
         height: 100vh;
         background-color: lightgreen;
-        background: url('https://images.unsplash.com/photo-1554384835-2bff08618ffb?ixlib=rb-1.2.1&auto=format&fit=crop&w=1834&q=80');
+        background-image: url('/img/backgrounds/login.jpg');
         /* https://images.unsplash.com/photo-1554585910-c42936acd8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80 */
         background-size: cover;
         background-position-y: 60%;
--- app/src/views/Username.vue
+++ app/src/views/Username.vue
@@ -125,7 +125,7 @@
     }
     .bg {
         height: 100vh;
-        background: url('https://images.unsplash.com/photo-1554585910-c42936acd8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80');
+        background-image: url('/img/backgrounds/username.jpg');
         background-size: cover;
         background-position-y: center;
         background-position-x: center;
