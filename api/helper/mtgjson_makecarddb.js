5adcb9199b540c32269f73efee5242ffba4c999a
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2020-06-14 22:07:32 -0400

    Fetching card images and storing them

--- api/helper/mtgjson_makecarddb.js
+++ api/helper/mtgjson_makecarddb.js
@@ -50,38 +50,92 @@ db.once('open', function() {
         console.log(keyTables.length);
 
         var i = 0;
-
         function myLoop() {
             setTimeout(function() {
-                console.log('this set:', keyTables[i], i);
-                console.log('this many cards', mtgjson[keyTables[i]].cards.length);
+                console.log('this set:', i, keyTables[i]);
+                console.log('Card count:', mtgjson[keyTables[i]].cards.length, 'cards');
+                
                 var j = 0;
                 function myInnerLoop() {
                     setTimeout(function() {
                         if (mtgjson[keyTables[i]].cards.length != 0) {
-                            console.log('this card:', mtgjson[keyTables[i]].cards[j].name);
+
+                            request(`https://api.scryfall.com/cards/${mtgjson[keyTables[i]].cards[j].scryfallId}`, { json: true }, (err, res, body) => {
+                                if (err) { return console.log(err); }
+                                // TODO: Get other image formats for mobile devices
+
+                                // SAVING TO DB
+                                const card = new Card(mtgjson[keyTables[i]].cards[j]);
+                                card.save(function (err, card) {
+                                    if (err) return console.error(err);
+
+                                    // DOWNLOADING CARD IMAGE
+                                    const download = (url, path, callback) => {
+                                        request.head(url, (err, res, body) => {
+                                        request(url)
+                                            .pipe(fs.createWriteStream(path))
+                                            .on('close', () => {callback(res)})
+                                        })
+                                    }
+
+                                    let url;
+                                    const path = `../images/cards/${card._id}.png`
+
+                                    if (body.image_uris) {
+                                        url = body.image_uris.png
+                                    }
+                                    else if (body.card_faces) {
+                                        //  Edge case for double faced card
+                                        for (let k = 0; k < body.card_faces.length; k++) {
+                                            if(body.card_faces[k].name === card.name) {
+                                                url = card_faces[k].image_uris.png;
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    else {
+                                        console.log('Something went wrong! We found nothing from scryfall!', body)
+                                    }
+
+                                    download(url, path, (res) => {
+                                        console.log(`✅ ${res.statusCode} => [${j}] - ${mtgjson[keyTables[i]].cards[j].name} from ${i} - ${keyTables[i]} fetched!`)
+                                        j++;
+                                        if (j < mtgjson[keyTables[i]].cards.length) {
+                                            myInnerLoop();
+                                        }
+                                        else {
+                                            i++;
+                                            if (i < keyTables.length) {
+                                                myLoop();
+                                            }
+                                            else {
+                                                console.log('Card database setup done!')
+                                            }
+                                        }
+                                    })
+                                });
+                            });
+
                         }
                         else {
                             // TODO: you will want to manage tokens at one point eventually right here
-                            console.log(mtgjson[keyTables[i]]);
-                        }
-                        
-                        j++;
-                        if (j < mtgjson[keyTables[i]].cards.length) {
-                            myInnerLoop();
-                        }
-                        else {
+                            // console.log(mtgjson[keyTables[i]]);
                             i++;
                             if (i < keyTables.length) {
                                 myLoop();
                             }
+                            else {
+                                console.log('Card database setup done!')
+                            }
                         }
-                    }, 500)
+                    }, 1000)
                 }
                 myInnerLoop();
-            }, 500)
+            }, 1000)
         }
         myLoop();
+        // console.log(mtgjson['DKA']);
+        
 
 // mtgjson[key].cards.length
             // for (var i = 0; i < 5; i++) {
@@ -106,7 +160,7 @@ db.once('open', function() {
             //     });
             // }
         // }
-        console.log("Done!");
+        // console.log("Done!");
     });
 });
 
